279e8a54e09120ed6d47699dc2ac83f60cbcd25c
江南 <jgnan1981@163.com>
2016-03-14 18:05:47 +0800

    fix rails dependencies

--- lib/wechat/jobs.rb
+++ lib/wechat/jobs.rb
@@ -4,7 +4,7 @@ module Wechat
     def self.update_token
       Rails.logger.debug {"[WechatJobs.update_token] updating wechat token"} if Rails.logger
       resp = RestClient.get "https://api.weixin.qq.com/cgi-bin/token",{params:{grant_type:'client_credential',appid:Wechat.app_id,secret:Wechat.app_secret}}
-      Rails.logger.info {"[WechatJobs.update_token] response -> #{resp}"} if Rails.info
+      Rails.logger.info {"[WechatJobs.update_token] response -> #{resp}"} if Rails.logger
       json = JSON.parse resp
       Wechat.access_token= json['access_token']
       Rails.logger.debug {"[WechatJobs.update_token] now the access_token updated to  -> #{Wechat.access_token}"} if Rails.logger
